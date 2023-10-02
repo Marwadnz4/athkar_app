5159f20212b07882e54232e7e8ed5e976f56f69f
Marwadnz4 <marwa.mohammed.jassim@gmail.com>
2023-10-02 20:00:08 +0300

    First commit

---
+++ lib/ui/screens/athkar_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class AthkarScreen extends StatelessWidget {
+  const AthkarScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
